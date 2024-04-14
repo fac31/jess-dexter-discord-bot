7eedd18ba098dc9f11ffc25206959e1183272357
Jess Pates <jesspates@hotmail.co.uk>
2024-04-14 17:49:39 +0100

    discord initialise added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 const key = process.env.OPEN_AI_KEY;
+const discordToken = process.env.DISCORD_KEY;
 
 const app = express();
 
@@ -13,3 +14,20 @@ app.get("/", (_req, res) => {
 app.listen(3000, () => {
     console.log("Listening on http://localhost:3000");
 });
+
+// Initialise Discord bot
+import { Client, GatewayIntentBits } from "discord.js";
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+        console.log("ping detected");
+    }
+});
+
+client.login(discordToken);
