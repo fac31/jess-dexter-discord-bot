f1a6468aec4896fb3b70cedf3e6704931255a6e4
DexterHill0 <49513414+DexterHill0@users.noreply.github.com>
2024-04-16 21:22:18 +0100

    add interactivity to message
    selecting game and player types updates the embed. once both are selected the start button is no longer disabled. after 60 seconds or the cancel button is pressed the game is cancelled

--- src/components/startGame.js
+++ src/components/startGame.js
@@ -5,7 +5,14 @@ import {
     ButtonBuilder,
     ActionRowBuilder,
     EmbedBuilder,
+    ComponentType,
 } from "discord.js";
+
+// this may be held somewhere else at a later date
+// it maps user id to another object holding the interaction data
+const currentStartGameInteractions = {};
+
+const GAME_TIMEOUT = 60_000;
 
 // an object containing the different ids used for different components
 // in case we need to access the components from an event somewhere else
@@ -16,25 +23,42 @@ const START_GAME_IDS = {
     CANCEL_BUTTON: "start-cancel",
 };
 
-const startGameEmbed = (interaction) => {
+const PLAYER_TYPE_VALUE_MAP = {
+    private: "Private",
+    public: "Public",
+};
+const GAME_TYPE_VALUE_MAP = {
+    guesser: "Guesser",
+    giver: "Clue Giver",
+};
+
+const startGameEmbed = (interaction, embedData = {}) => {
     const embed = new EmbedBuilder()
         .setColor(0xffa600)
         .setAuthor({
             name: `${interaction.user.globalName}'s HeadsUp Game`,
             iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png?size=256`,
         })
-        .setTitle("Game ID")
-        .setDescription("<game id>")
+        .setTitle("Game Id")
+        .setDescription(embedData.gameId ?? "-")
         .addFields(
-            { name: "Game Type", value: "-", inline: true },
-            { name: "Player Type", value: "-", inline: true }
+            {
+                name: "Game Type",
+                value: embedData.gameType ?? "-",
+                inline: true,
+            },
+            {
+                name: "Player Type",
+                value: embedData.playerType ?? "-",
+                inline: true,
+            }
         )
         .setTimestamp();
 
     return embed;
 };
 
-export const startGameComponent = async (interaction) => {
+const startGameComponents = (data = {}) => {
     const gameTypeSelect = new StringSelectMenuBuilder()
         .setCustomId(START_GAME_IDS.GAME_TYPE_SELECT)
         .setPlaceholder("Game Type")
@@ -42,7 +66,7 @@ export const startGameComponent = async (interaction) => {
             new StringSelectMenuOptionBuilder()
                 .setLabel("Private")
                 .setDescription(
-                    "The game can only be joined by a person you give the game ID to."
+                    "The game can only be joined by a person you give the game Id to."
                 )
                 .setValue("private"),
             new StringSelectMenuOptionBuilder()
@@ -81,7 +105,7 @@ export const startGameComponent = async (interaction) => {
         .setCustomId(START_GAME_IDS.CONFIRM_BUTTON)
         .setLabel("Start Game!")
         .setStyle(ButtonStyle.Primary)
-        .setDisabled(true);
+        .setDisabled(data.startDisabled ?? true);
 
     const cancel = new ButtonBuilder()
         .setCustomId(START_GAME_IDS.CANCEL_BUTTON)
@@ -90,12 +114,112 @@ export const startGameComponent = async (interaction) => {
 
     const buttonRow = new ActionRowBuilder().addComponents(cancel, confirm);
 
-    const embed = startGameEmbed(interaction);
+    return [playerTypeRow, gameTypeRow, buttonRow];
+};
 
-    await interaction.reply({
-        components: [gameTypeRow, playerTypeRow, buttonRow],
+const cancelGame = (interaction) => {
+    delete currentStartGameInteractions[interaction.user.id];
+};
+
+export const startGameComponent = async (interaction) => {
+    const components = startGameComponents();
+
+    const message = await interaction.reply({
+        components: components,
         ephemeral: true,
-        embeds: [embed],
+        embeds: [startGameEmbed(interaction)],
         fetchReply: true,
+    });
+
+    currentStartGameInteractions[interaction.user.id] = {
+        embedData: { gameType: null, playerType: null, gameId: null },
+        buttonData: { startDisabled: true },
+    };
+
+    // this function will ignore any events that arent by the current user
+    // may not be neccessary as the message is ephemeral but better to be safe
+    const collectorFilter = (i) => {
+        // this stops the error showing that the interaction failed
+        // shouldnt need it since we edit but discord thinks we arent doing anything
+        i.deferUpdate();
+        return i.user.id === interaction.user.id;
+    };
+    // listenes for the string select drop downs to be changed and emits and event if they do
+    const selectCollector = message.createMessageComponentCollector({
+        filter: collectorFilter,
+        componentType: ComponentType.StringSelect,
+        time: GAME_TIMEOUT,
+    });
+    const buttonCollector = message.createMessageComponentCollector({
+        filter: collectorFilter,
+        componentType: ComponentType.Button,
+        time: GAME_TIMEOUT,
+    });
+
+    selectCollector.on("collect", (selectInteraction) => {
+        const currentUserData =
+            currentStartGameInteractions[interaction.user.id];
+
+        if (selectInteraction.customId == START_GAME_IDS.GAME_TYPE_SELECT) {
+            const selectedType = selectInteraction.values[0];
+
+            // update the current user's embed data so the embed will change
+            // and will stay changed if they update a different property
+            currentUserData.embedData.gameType =
+                PLAYER_TYPE_VALUE_MAP[selectedType];
+
+            interaction.editReply({
+                embeds: [
+                    startGameEmbed(interaction, currentUserData.embedData),
+                ],
+            });
+        } else if (
+            selectInteraction.customId == START_GAME_IDS.PLAYER_TYPE_SELECT
+        ) {
+            const selectedType = selectInteraction.values[0];
+
+            currentUserData.embedData.playerType =
+                GAME_TYPE_VALUE_MAP[selectedType];
+
+            interaction.editReply({
+                embeds: [
+                    startGameEmbed(interaction, currentUserData.embedData),
+                ],
+            });
+        }
+
+        if (
+            currentUserData.embedData.gameType != undefined &&
+            currentUserData.embedData.playerType != undefined
+        ) {
+            currentUserData.buttonData.startDisabled = false;
+            interaction.editReply({
+                components: startGameComponents(currentUserData.buttonData),
+            });
+        }
+    });
+
+    buttonCollector.on("collect", (buttonInteration) => {
+        if (buttonInteration.customId == START_GAME_IDS.CANCEL_BUTTON) {
+            interaction.editReply({
+                embeds: [],
+                components: [],
+                content: "Game was cancelled!",
+            });
+
+            cancelGame(interaction);
+        }
+    });
+
+    // if the user takes too long we edit the message and
+    // delete their data
+    selectCollector.on("end", () => {
+        interaction.editReply({
+            embeds: [],
+            components: [],
+            content: "Game took too long to start!",
+        });
+
+        cancelGame(interaction);
     });
 };
